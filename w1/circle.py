8c8d6cb4572afbc324e4d4411ecf7c5453766be5
Rahul Chalumuri <rahulchalumuri54@gmail.com>
2018-07-11 16:04:22 -0400

    allows racecar to complete circle via 360°

---
+++ w1/circle.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+import rospy
+from ackermann_msgs.msg import AckermannDriveStamped
+
+PI = 3.141
+
+def forward():
+        # Create a node to publish information with
+        rospy.init_node('forwardnode', anonymous=True)
+        print("Initialized node")
+        # Publish to the ackermann drive node
+        pub = rospy.Publisher('/vesc/high_level/ackermann_cmd_mux/input/nav_0/', AckermannDriveStamped, queue_size=10)
+        print("Initialized publisher")
+        # Set the rate at which to send the message
+        rate = rospy.Rate(10) 
+        drivemsg = AckermannDriveStamped()
+        while not rospy.is_shutdown():
+                drivemsg.drive.speed = 1.0
+                drivemsg.drive.steering_angle = PI/6
+                # Publish information and sleep
+                pub.publish(drivemsg) 
+                rate.sleep()
+
+if __name__ == '__main__':
+        try:
+                forward()
+        except rospy.ROSInterruptException:
+                pass
